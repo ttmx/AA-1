f18caa8c3d8a05fe422f03b8d794a2a8cf5bf551
André Palma <andremarquespalma@gmail.com>
2021-11-10 19:18:32 +0000

    Add bandwidth Tuning

--- TP1.py
+++ TP1.py
@@ -1,26 +1,50 @@
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KernelDensity
-from numpy import loadtxt
 import numpy as np
-from sklearn import metrics
+from KernelDensityNB import KernelDensityNB
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.model_selection import StratifiedKFold
+
 
 def standardize(df):
-    return (df-df.mean())/df.std()
+    return (df - df.mean()) / df.std()
+
 
 def load_data(file_name):
-    data = loadtxt(file_name,delimiter="\t")
-    data[:,:-1] = standardize(data[:,:-1])
+    data = np.loadtxt(file_name, delimiter="\t")
+    data[:, :-1] = standardize(data[:, :-1])
     np.random.shuffle(data)
     return data
 
 
-test = load_data("./TP1_test.tsv")
-train = load_data("./TP1_train.tsv")
+def nb_classifier(nb):
+    avg_error = 0
+    skf = StratifiedKFold(n_splits=folds)
+    for train_i, validation_i in skf.split(train[:, :-1], train[:, -1]):
+        train_set = train[train_i]
+        validation_set = train[validation_i]
+        nb.fit(train_set[:, :-1], train_set[:, -1])
+        error = 1 - nb.score(validation_set[:, :-1], validation_set[:, -1])
+        avg_error += error
+    avg_error /= folds
+    return avg_error
 
-nb = GaussianNB()
-print(test)
-f = nb.fit(train[: , :4],train[:,-1])
 
-pred = nb.predict(test[:,:4])
-met = metrics.accuracy_score(pred,test[:,-1])
-print(met)
+if __name__ == '__main__':
+    test = load_data("./TP1_test.tsv")
+    train = load_data("./TP1_train.tsv")
+
+    folds = 5
+
+    min_error = 1
+    best_bandwidth = -1
+    for bandwidth in np.linspace(0.02, 6, 300):
+        error = nb_classifier(KernelDensityNB(bandwidth))
+        if error < min_error:
+            min_error = error
+            best_bandwidth = bandwidth
+
+    print(f"Kernel Density validation error: {min_error} with bandwidth: {best_bandwidth}")
+
+    print(f"Gaussian validation error: {nb_classifier(GaussianNB())}")
+
+    clf = SVC(kernel='rbf')
