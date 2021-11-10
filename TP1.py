b106ec69c4275823980624b6e7006a98e474a5c0
André Palma <andremarquespalma@gmail.com>
2021-11-10 21:30:10 +0000

    Add Approximate Normal Test

--- TP1.py
+++ TP1.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from KernelDensityNB import KernelDensityNB
 from sklearn.naive_bayes import GaussianNB
@@ -16,17 +17,28 @@ def load_data(file_name):
     return data
 
 
-def nb_classifier(nb):
+def cross_validation(nb):
     avg_error = 0
     skf = StratifiedKFold(n_splits=folds)
     for train_i, validation_i in skf.split(train[:, :-1], train[:, -1]):
         train_set = train[train_i]
         validation_set = train[validation_i]
-        nb.fit(train_set[:, :-1], train_set[:, -1])
-        error = 1 - nb.score(validation_set[:, :-1], validation_set[:, -1])
-        avg_error += error
+        avg_error += fit_and_score(nb, train_set, validation_set)
     avg_error /= folds
     return avg_error
+
+
+def fit_and_score(nb, fit, score):
+    nb.fit(fit[:, :-1], fit[:, -1])
+    error = 1 - nb.score(score[:, :-1], score[:, -1])
+    return error
+
+
+def approximate_normal_test(test_error):
+    sigma = math.sqrt(test.size * test_error * (1 - test_error))
+    absolute_margin = 1.96 * sigma  # 0.95 confidence interval
+    relative_margin = absolute_margin / test.size
+    return test_error, relative_margin
 
 
 if __name__ == '__main__':
@@ -38,21 +50,30 @@ if __name__ == '__main__':
     min_error = 1
     best_bandwidth = -1
     for bandwidth in np.linspace(0.02, 6, 300):
-        error = nb_classifier(KernelDensityNB(bandwidth))
+        error = cross_validation(KernelDensityNB(bandwidth))
         if error < min_error:
             min_error = error
             best_bandwidth = bandwidth
 
     print(f"Kernel Density validation error: {min_error} with bandwidth: {best_bandwidth}")
 
-    print(f"Gaussian validation error: {nb_classifier(GaussianNB())}")
+    print(f"Gaussian validation error: {cross_validation(GaussianNB())}")
 
     min_error = 1
     best_gamma = -1
     for gamma in np.linspace(0.2, 6, 30):
-        error = nb_classifier(SVC(gamma=gamma, kernel="rbf"))
+        error = cross_validation(SVC(gamma=gamma, kernel="rbf"))
         if error < min_error:
             min_error = error
             best_gamma = gamma
 
     print(f"SVM validation error: {min_error} with gamma: {best_gamma}")
+
+    K_error, K_margin = approximate_normal_test(fit_and_score(KernelDensityNB(best_bandwidth), train, test))
+    print(f"Kernel Density test error: {K_error}% +- {K_margin}% with bandwidth: {best_bandwidth}")
+
+    G_error, G_margin = approximate_normal_test(fit_and_score(GaussianNB(), train, test))
+    print(f"Gaussian test error: {G_error}% +- {G_margin}%")
+
+    SVM_error, SVM_margin = approximate_normal_test(fit_and_score(SVC(gamma=best_gamma, kernel="rbf"), train, test))
+    print(f"SVM test error: {SVM_error}% +- {SVM_margin}% with gamma: {best_gamma}")
