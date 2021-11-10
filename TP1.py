20e9699591f36cabda7f654fd4f2cef844c8862e
Tiago Teles <tiago.sequeira.teles@gmail.com>
2021-11-10 13:29:53 +0000

    Changed to np from pandas

--- TP1.py
+++ TP1.py
@@ -1,16 +1,21 @@
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KernelDensity
-from pandas import read_csv
+from numpy import loadtxt
 from sklearn import metrics
 
-test = read_csv("./TP1_test.tsv",sep="\t")
-train = read_csv("./TP1_train.tsv",sep="\t")
+def standardize(df):
+    return (df-df.mean())/df.std()
+
+test = loadtxt("./TP1_test.tsv",delimiter="\t")
+test[:,:-1] = standardize(test[:,:-1])
+train = loadtxt("./TP1_train.tsv",delimiter="\t")
+train[:,:-1] = standardize(train[:,:-1])
 
 nb = GaussianNB()
 
-print(test.iloc[:,-1:])
-f = nb.fit(train.iloc[: , :4],train.iloc[:,-1:].values.ravel())
+print(test)
+f = nb.fit(train[: , :4],train[:,-1:].ravel())
 
-pred = nb.predict(test.iloc[:,:4])
-met = metrics.accuracy_score(pred,test.iloc[:,-1])
+pred = nb.predict(test[:,:4])
+met = metrics.accuracy_score(pred,test[:,-1])
 print(met)
